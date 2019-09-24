e0796409aaab5468ddd6990aa4bbf2995f78be4b
Tim Satke <tim.satke@gmail.com>
2019-09-24 14:54:32 +0200

    Removed parser because of no experience and poor architecture

--- examples/json/main.go
+++ examples/json/main.go
@@ -4,11 +4,10 @@ import (
 	"log"
 
 	"github.com/TimSatke/parser/lexer"
-	"github.com/TimSatke/parser/parser"
 )
 
 const data = `
-{a
+{
 	"foo": "bar",
 	"sna": true,
 	"abc": [
@@ -29,16 +28,7 @@ const data = `
 func main() {
 	l := lexer.New([]byte(data), lexJSON)
 	go l.StartLexing()
-
-	p := parser.New(l, parseJSON, TokenUnknown, TokenError)
-
-	err := p.Parse()
-	if err != nil {
-		log.Fatal(err)
+	for token := range l.TokenStream().Tokens() {
+		log.Printf("token: %v\n", token.String())
 	}
-}
-
-func parseJSON(p parser.Parser) parser.Rule {
-	p.Accept(TokenWhitespace, TokenBracketOpen, TokenString)
-	return parseJSON
 }
